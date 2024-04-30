2d654051a178d2119a925118a1bd1285311fd3da
Ryan Soury <ryan@webdoodle.com.au>
2024-04-30 23:58:02 +1000

    commit redact

--- host/src/main.rs
+++ host/src/main.rs
@@ -1,53 +1,28 @@
 use core::str;
 
 use methods::ZKAF_ELF;
-use p256::pkcs8::DecodePublicKey;
 use risc0_zkvm::{default_prover, ExecutorEnv};
-use tlsn_core::proof::{SessionProof, TlsProof};
+use serde::{Deserialize, Serialize};
 
 
 use tlsn_substrings_verifier::{self, proof::{SessionHeader, SubstringsProof}};
+#[derive(Serialize, Deserialize, Debug)]
+struct ZkParam {
+    header: SessionHeader,
+    substrings: SubstringsProof,
+}
 
 fn main() {
-    // Deserialize the proof
-    let proof = std::fs::read_to_string("host/fixtures/proof.json").unwrap();
-    let proof: TlsProof = serde_json::from_str(proof.as_str()).unwrap();
-
-    let TlsProof {
-        // The session proof establishes the identity of the server and the commitments
-        // to the TLS transcript.
-        session,
-        // The substrings proof proves select portions of the transcript, while redacting
-        // anything the Prover chose not to disclose.
-        substrings,
-    } = proof;
-
-    // Verify the session proof against the Notary's public key
-    //
-    // This verifies the identity of the server using a default certificate verifier which trusts
-    // the root certificates from the `webpki-roots` crate.
-    session
-        .verify_with_default_cert_verifier(notary_pubkey())
-        .unwrap();
-
-    let SessionProof {
-        // The session header that was signed by the Notary is a succinct commitment to the TLS transcript.
-        header,
-        ..
-    } = session;
-
-
-    // type conversion occurs here
-    // we need to convert from the tlsn core definitions to the definitions from the verifier
-    let verifier_parsed_header: SessionHeader = serde_json::from_str(&serde_json::to_string(&header).unwrap()).unwrap();
-    let verifier_parsed_substrings: SubstringsProof = serde_json::from_str(&serde_json::to_string(&substrings).expect("Serialization failed")).unwrap();
-
-    // pass the input to the guest code
-    let input: (SessionHeader, SubstringsProof) = (verifier_parsed_header, verifier_parsed_substrings);
-    let env = ExecutorEnv::builder().write(&input).unwrap().build().unwrap();
-
     // Obtain the default prover.
     let prover = default_prover();
+
+    // read in th einputs from json
+    let proof_params = std::fs::read_to_string("./host/fixtures/zk_params.json").unwrap();
+    let proof_params: ZkParam = serde_json::from_str(proof_params.as_str()).unwrap();
+
+    // pass the input to the guest code
+    let input: (SessionHeader, SubstringsProof) = (proof_params.header, proof_params.substrings);
+    let env = ExecutorEnv::builder().write(&input).unwrap().build().unwrap();
 
     // Produce a receipt by proving the specified ELF binary.
     let receipt = prover.prove(env, ZKAF_ELF).unwrap();
@@ -57,12 +32,6 @@ fn main() {
 
     // Print, notice, after committing to a journal, the private input became public
     println!("I generated a proof of guest execution!");
-    println!("Request: \n{}", request);
-    println!("Response: \n {}", response);
-}
-
-/// Returns a Notary pubkey trusted by this Verifier
-fn notary_pubkey() -> p256::PublicKey {
-    let pem_file = str::from_utf8(include_bytes!("../fixtures/notary.pub")).unwrap();
-    p256::PublicKey::from_public_key_pem(pem_file).unwrap()
+    println!("Request Verified: \n{}", request);
+    println!("Response Verified: \n {}", response);
 }
