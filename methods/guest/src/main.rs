3f2d87720c22019237e12566a6fdc268fd9c4707
Ryan Soury <ryan@webdoodle.com.au>
2024-04-30 23:58:02 +1000

    commit redact

--- methods/guest/src/main.rs
+++ methods/guest/src/main.rs
@@ -8,11 +8,11 @@ risc0_zkvm::guest::entry!(main);
 fn main() {
     // read the substring
     let (session_header, substrings): (SessionHeader, SubstringsProof) = env::read();
-    let (mut sent, mut recv) = substrings.verify(&session_header).unwrap();
+    let (sent, recv) = substrings.verify(&session_header).unwrap();
 
     // set redacted string value
-    sent.set_redacted(b'X');
-    recv.set_redacted(b'X');
+    // sent.set_redacted(b'X');
+    // recv.set_redacted(b'X');
 
     // recover the request and response
     let request = String::from_utf8(sent.data().to_vec()).unwrap();
