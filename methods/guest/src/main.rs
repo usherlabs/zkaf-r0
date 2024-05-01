819e5bd10fa2408757a3aa97b5e8dc9f79f0f345
xander <shuaibualexander@gmail.com>
2024-05-01 13:12:11 +0100

    remove stale comments

--- methods/guest/src/main.rs
+++ methods/guest/src/main.rs
@@ -16,23 +16,10 @@ fn main() {
     
     let (sent, recv) = substrings.verify(&session_header).unwrap();
 
-    // set redacted string value
-    // sent.set_redacted(b'X');
-    // recv.set_redacted(b'X');
-
-    // recover the request and response
-    // let request = String::from_utf8(sent.data().to_vec()).unwrap();
-    // let response = String::from_utf8(recv.data().to_vec()).unwrap();
-
     // Log that we've successfully recovered the request and response...
     let is_req = !sent.data().to_vec().is_empty();
     let is_res = !recv.data().to_vec().is_empty();
 
     env::log("committing results to journal");
     env::commit(&(is_req, is_res));
-
-    // // write request and response to the journal public output
-    // env::log("committing data to journal");
-    // env::commit(&(request, response));
-    // env::log("committed data to journal");
 }
