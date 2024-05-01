cbf2c02e797071fcccf58a6b4dd6d1191c92c8fd
Ryan Soury <ryan@webdoodle.com.au>
2024-05-01 23:34:53 +1000

    rm ws

--- methods/guest/src/main.rs
+++ methods/guest/src/main.rs
@@ -8,12 +8,12 @@ risc0_zkvm::guest::entry!(main);
 fn main() {
     // read the substring
     let (session_header,  substrings): (String, String) = env::read();
-    
+
     // handle deserialization manually
     // ? more efficiently pass bytes instead of strings?
     let session_header: SessionHeader = serde_json::from_str(&session_header).unwrap();
     let substrings: SubstringsProof = serde_json::from_str(&substrings).unwrap();
-    
+
     let (sent, recv) = substrings.verify(&session_header).unwrap();
 
     // Log that we've successfully recovered the request and response...
